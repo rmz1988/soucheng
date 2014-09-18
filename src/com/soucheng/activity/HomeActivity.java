a09b969480aa60a7a33a9643ca21675a9cdb9b18
= <=>
2014-09-18 14:30:09 +0800

    创建搜城项目

---
+++ src/com/soucheng/activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.soucheng.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 首页
+ */
+public class HomeActivity extends Activity {
+
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
---
+++ src/com/soucheng/application/MainApplication.java
@@ -0,0 +1,16 @@
+package com.soucheng.application;
+
+import android.app.Application;
+
+/**
+ * Application记录公共资源
+ *
+ * @author lichen
+ */
+public class MainApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
