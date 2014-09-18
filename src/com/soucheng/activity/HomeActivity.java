76fa0af57c125a439526383f73d9a4b063896351
= <=>
2014-09-18 15:01:26 +0800

    创建搜城项目

--- src/com/soucheng/activity/HomeActivity.java
+++ src/com/soucheng/activity/HomeActivity.java
@@ -14,6 +14,6 @@ public class HomeActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.home);
 	}
 }
