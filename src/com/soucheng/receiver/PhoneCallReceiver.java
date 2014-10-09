5e2b6f0d71c1ac9da43dd8bee23374c447cf8537
= <=>
2014-10-09 17:44:47 +0800

    修改通话弹窗功能

--- src/com/soucheng/receiver/PhoneCallReceiver.java
+++ src/com/soucheng/receiver/PhoneCallReceiver.java
@@ -13,50 +13,58 @@ import com.soucheng.dialog.PhoneCallDialog;
  */
 public class PhoneCallReceiver extends BroadcastReceiver {
 
-    private PhoneCallDialog dialog;
-    private static boolean hasShown;
+	private PhoneCallDialog dialog;
+	private static boolean hasShown;
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (dialog == null) {
-            dialog = new PhoneCallDialog(context);
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	   /* if (dialog == null) {
+	        dialog = new PhoneCallDialog(context);
         }
         if (!dialog.isShowing() && !hasShown) {
             dialog = new PhoneCallDialog(context);
             dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
             dialog.show();
             hasShown = true;
-        }
+        }*/
 
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        //设置一个监听器
-        manager.listen(new SouchengPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+		TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		//设置一个监听器
+		manager.listen(new SouchengPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
 
 
-    }
+	}
 
-    private class SouchengPhoneStateListener extends PhoneStateListener {
-        private Context context;
+	private class SouchengPhoneStateListener extends PhoneStateListener {
 
-        public SouchengPhoneStateListener(Context context) {
-            this.context = context;
-        }
+		private Context context;
 
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            super.onCallStateChanged(state, incomingNumber);
-            switch (state) {
-                /*case TelephonyManager.CALL_STATE_RINGING:
+		public SouchengPhoneStateListener(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			super.onCallStateChanged(state, incomingNumber);
+			switch (state) {
+	            /*case TelephonyManager.CALL_STATE_RINGING:
                     dialog = new PhoneCallDialog(context);
                     dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
                     dialog.show();
                     break;*/
-                case TelephonyManager.CALL_STATE_IDLE:
-                    hasShown = false;
-                    if (dialog != null)
-                        dialog.cancel();
-                    break;
-            }
-        }
-    }
+				case TelephonyManager.CALL_STATE_IDLE:
+					if (dialog == null) {
+						dialog = new PhoneCallDialog(context);
+					}
+
+					if(!dialog.isShowing() && !hasShown){
+						dialog = new PhoneCallDialog(context);
+						dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+						dialog.show();
+						hasShown = true;
+					}
+					break;
+			}
+		}
+	}
 }
